7bb6264faaecdaa2d64eb4986aa9bbf59106c55a
harkamal <gajinder@g11.in>
2022-09-07 22:49:18 +0530

    add persistence to the keymanager proposer updates

--- packages/cli/src/cmds/validator/paths.ts
+++ packages/cli/src/cmds/validator/paths.ts
@@ -10,6 +10,7 @@ export type AccountPaths = {
   keystoresDir: string;
   secretsDir: string;
   remoteKeysDir: string;
+  proposerDir: string;
 };
 
 /**
@@ -74,11 +75,13 @@ export function getAccountPaths(
   const keystoresDir = args.keystoresDir || path.join(dataDir, "keystores");
   const secretsDir = args.secretsDir || path.join(dataDir, "secrets");
   const remoteKeysDir = args.remoteKeysDir || path.join(dataDir, "remoteKeys");
+  const proposerDir = args.proposerDir || path.join(dataDir, "proposerConfigs");
   return {
     ...globalPaths,
     keystoresDir,
     secretsDir,
     remoteKeysDir,
+    proposerDir,
   };
 }
 
